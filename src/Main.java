cfaac101c8290ec59d1020aabfc4185f56017b7a
Onilla <dnd92@mail.ru>
2024-02-09 22:25:15 +0300

    one commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 import domain.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
