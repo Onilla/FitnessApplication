e6d0d9513a2d5460db64f8a19434c947d036a1ea
Onilla <dnd92@mail.ru>
2024-02-17 22:51:27 +0300

    six commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import domain.*;
+
 import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        Client client = new Client("Ivan","Ivanov",1990);
+        Client client = new Client("Ivan", "Ivanov", 1990);
         Membership membershipIvan = new Membership(LocalDate.now(),
-                LocalDate.of(2025,2,6), client, TypeTicket.SINGLE_TICKET);
-                Client client2 = new Client("Petr","Petrov",1992);
-          Membership membershipPetr = new Membership(LocalDate.now(),
-                LocalDate.of(2025,2,6), client2, TypeTicket.FULL_TICKET);
-          Fitness fitness = new Fitness();
-          fitness.clientRegistration(membershipIvan, TrainingZone.GYM);
-          fitness.clientRegistration(membershipPetr, TrainingZone.WORKOUT);
-          fitness.printListOfMembership();
-
+                LocalDate.of(2025, 2, 6), client, TypeTicket.SINGLE_TICKET);
+        Client client2 = new Client("Petr", "Petrov", 1992);
+        Membership membershipPetr = new Membership(LocalDate.now(),
+                LocalDate.of(2025, 2, 6), client2, TypeTicket.FULL_TICKET);
+        Fitness fitness = new Fitness();
+        fitness.clientRegistration(membershipIvan, TrainingZone.GYM);
+        fitness.clientRegistration(membershipPetr, TrainingZone.WORKOUT);
+        fitness.printListOfMembership();
     }
 }
 
--- src/domain/Fitness.java
+++ src/domain/Fitness.java
@@ -1,74 +1,90 @@
 package domain;
+
 import java.time.LocalDate;
+
 import java.time.LocalTime;
 
 import java.util.ArrayList;
+
 import java.util.Date;
+
 import java.util.List;
+
 public class Fitness {
-    private final List<String> membershipGym = new ArrayList<>();
     final int MAX_CLIENT_IN_ZONE = 20;
+    private final List<String> membershipGym = new ArrayList<>();
     private final List<String> membershipSwimPool = new ArrayList<>();
     private final List<String> membershipWorkout = new ArrayList<>();
     private final LocalDate currentDate = LocalDate.now();
     private final LocalTime dailyTicketTime = LocalTime.of(16, 0);
     private final LocalTime currentTime = LocalTime.now();
     private final LocalTime openTime = LocalTime.of(8, 0);
-    private final LocalTime closeTime = LocalTime.of(23, 0);
-        public void clientRegistration(Membership membership, TrainingZone zone) {
-            if (checkTimeVisit()) {
-                System.out.println("Время посещения клуба с 08:00 до 22:00. Приходите в другое время");
-            } else if (checkDateReg(membership.getEndDate())) {
-                System.out.println("Абонемент просрочен");
-            } else if (checkTypeTicket(membership)) {
-                System.out.println("Доступ для дневного абонемента запрещен после 16:00");
-            } else if (checkMaxClient()) {
-                System.out.println(zone.getTranslation() + " переполнен");
-            } else if (checkClientInZone(membership, zone)) {
-                System.out.println("Абонемент зарегистрирован в другой зоне");
-            } else if(checkZoneClient(membership,zone)) {
-                System.out.println("Ваш абонемент не позволяет пройти в эту зону");
-                } else {
-                addClientAtList(membership, zone);
-                printInfo(membership,zone);
-            }
+    private final LocalTime closeTime = LocalTime.of(22, 10);
+
+    public void clientRegistration(Membership membership, TrainingZone zone) {
+        if (checkTimeVisit()) {
+            System.out.println("Время посещения клуба с 08:00 до 22:00. Приходите в другое время");
+        } else if (checkDateReg(membership.getEndDate())) {
+            System.out.println("Абонемент просрочен");
+        } else if (checkTypeTicket(membership)) {
+            System.out.println("Доступ для дневного абонемента запрещен после 16:00");
+        } else if (checkMaxClient()) {
+            System.out.println(zone.getTranslation() + " переполнен");
+        } else if (checkClientInZone(membership, zone)) {
+            System.out.println("Абонемент зарегистрирован в другой зоне");
+        } else if (checkZoneClient(membership, zone)) {
+            System.out.println("Ваш абонемент не позволяет пройти в эту зону");
+        } else {
+            addClientAtList(membership, zone);
+            printInfo(membership, zone);
         }
-        public void addClientAtList(Membership membership, TrainingZone zone){
-            switch (zone){
-                case GYM -> membershipGym.add(membership.getOwner().getName() + " "
-                        + membership.getOwner().getSurName());
-                case SWIM_POOL -> membershipSwimPool.add(membership.getOwner().getName() + " "
-                        + membership.getOwner().getSurName());
-                case WORKOUT -> membershipWorkout.add(membership.getOwner().getName() + " "
-                        + membership.getOwner().getSurName());
-            }
+    }
+
+    public void addClientAtList(Membership membership, TrainingZone zone) {
+        switch (zone) {
+            case GYM -> membershipGym.add(membership.getOwner().getName() +
+                    " " + membership.getOwner().getSurName());
+            case SWIM_POOL -> membershipSwimPool.add(membership.getOwner().getName() +
+                    " " + membership.getOwner().getSurName());
+            case WORKOUT -> membershipWorkout.add(membership.getOwner().getName() +
+                    " " + membership.getOwner().getSurName());
         }
-        public void printInfo(Membership membership, TrainingZone zone) {
-        System.out.println("Добро пожаловать " + "\nКлиент: " + membership.getOwner().getName() + " " + membership.getOwner().getSurName() +
+    }
+
+    public void printInfo(Membership membership, TrainingZone zone) {
+        System.out.println("Добро пожаловать " + "\nКлиент: " + membership.getOwner().getName() +
+                " " + membership.getOwner().getSurName() +
                 " " + membership.getOwner().getBirthYear() + " года рождения" +
                 "\nТренировочная зона: " + zone.getTranslation() +
                 "\nДата и время посещения: " + new Date());
-        }
-    public boolean checkTypeTicket(Membership membership){
-            return (((membership.getTypeTicket()).equals(TypeTicket.DAILY_TICKET)) &&
-                    (currentTime.isAfter(dailyTicketTime)));
+        goHome();
     }
+
+    public boolean checkTypeTicket(Membership membership) {
+        return (((membership.getTypeTicket()).equals(TypeTicket.DAILY_TICKET)) &&
+                (currentTime.isAfter(dailyTicketTime)));
+    }
+
     public void printListOfMembership() {
-        System.out.println("\nВ данный момент занимаются в тренажерном зале:" + membershipGym +
+        System.out.println("В данный момент занимаются в тренажерном зале:" + membershipGym +
                 "\nВ данный момент занимаются в бассене:" + membershipSwimPool +
-                "\nВ данный момент занимаются на групповых занятиях:" + membershipWorkout);}
+                "\nВ данный момент занимаются на групповых занятиях:" + membershipWorkout);
+    }
+
     public boolean checkMaxClient() {
         return ((membershipGym.size() > MAX_CLIENT_IN_ZONE) |
                 (membershipSwimPool.size() > MAX_CLIENT_IN_ZONE) |
                 (membershipWorkout.size() > MAX_CLIENT_IN_ZONE));
     }
-    public boolean checkZoneClient(Membership membership,TrainingZone zone){
+
+    public boolean checkZoneClient(Membership membership, TrainingZone zone) {
         return switch (zone) {
             case GYM -> false;
             case SWIM_POOL -> ((membership.ticket).equals(TypeTicket.DAILY_TICKET));
             case WORKOUT -> ((membership.ticket).equals(TypeTicket.SINGLE_TICKET));
         };
     }
+
     public boolean checkClientInZone(Membership membership, TrainingZone zone) {
         return switch (zone) {
             case GYM -> ((membershipSwimPool.contains(membership.getOwner().getName() + " " +
@@ -85,10 +101,20 @@ public class Fitness {
                     membership.getOwner().getSurName())));
         };
     }
+
     public boolean checkTimeVisit() {
-        return  (currentTime.isBefore(openTime) || (currentTime.isAfter(closeTime)));
+        return (currentTime.isBefore(openTime) || (currentTime.isAfter(closeTime)));
     }
+
     public boolean checkDateReg(LocalDate endDate) {
-        return  (currentDate.isAfter(endDate));
+        return (currentDate.isAfter(endDate));
+    }
+
+    public void goHome() {
+        if (LocalTime.now().isAfter(closeTime)) {
+            membershipGym.clear();
+            membershipSwimPool.clear();
+            membershipWorkout.clear();
         }
     }
+}
